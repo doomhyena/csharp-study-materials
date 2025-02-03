5272c2b1da1f8553e6567b6038989bf5fdd3df9f
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2025-02-03 10:32:41 +0100

    FourtPorject
    This is the 4. Project of the C# Study Material

---
+++ Porjects/fourthproject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fourthproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valtozo deklaralasa
+            int ora = 0;
+            
+            Console.WriteLine("Kerek egy szamot: ");
+            
+            ora = int.Parse(Console.ReadLine());
+
+            if (ora >= 0 && ora <= 5) {
+                Console.WriteLine("Jo reggelt!");
+            } else if (ora > 8 && ora <= 10) {
+                Console.WriteLine("Szep napot!!");
+            } else {
+                Console.WriteLine("Szep Jo estet!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
