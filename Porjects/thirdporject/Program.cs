7f05d865aaf06d0a0e459ef6ae4a5db9cb58749e
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2025-02-04 14:14:30 +0100

    Third Project
    This is the third project.

---
+++ Porjects/thirdporject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace thirdproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keresztnev = "";
+            string vezeteknev = "";
+            string monogram = "";
+
+            Console.Write("Kérem a Vezetéknevét: ");
+            vezeteknev = Console.ReadLine();
+            Console.Write("Kérem a Keresztnevét: ");
+            keresztnev = Console.ReadLine();
+
+            monogram = vezeteknev[0].ToString();
+            monogram += keresztnev[0].ToString();
+
+            Console.Write("A monogram: ");
+            Console.WriteLine(monogram);
+            Console.ReadKey();
+
+        }
+    }
+}
