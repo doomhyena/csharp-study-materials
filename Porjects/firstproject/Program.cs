f198743e0f994890608e60ce03bea0ade44d65ff
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2025-02-04 14:07:04 +0100

    First project
    This is the first project

---
+++ Porjects/firstproject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fristproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int szam1;
+            int szam2;
+            int sum = 0;
+            
+            Console.Write("Kerem az elso szamot: ");
+            szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Az elso szam erteke: {szam1}");
+            Console.Write("Kerem az masodik szamot: ");
+            szam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A masodik szam erteke: {szam2}");
+
+            sum = szam1 + szam2;
+            Console.WriteLine($"A ket szam osszege: {sum}");
+            
+            Console.ReadKey();
+        }
+    }
+}
