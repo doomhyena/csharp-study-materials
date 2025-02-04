b55afc80e6597ee3310310589e52605b78aba9fb
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2025-02-04 14:13:41 +0100

    Second Project
    This is the second project.

---
+++ Porjects/secondproject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace secondproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double atlag = 0.00;
+            double szorzata = 1;
+            for (int i = 0; i <= 10; i++)
+            {
+                sum += i;
+            }
+            atlag = sum / 10;
+            int j = 1;
+            while (j <= 10)
+            {
+                szorzata = szorzata * j;
+                j++;
+            }
+
+            Console.WriteLine("Számok összege:");
+            Console.WriteLine(sum);
+            Console.WriteLine("Számok átlaga:");
+            Console.WriteLine(atlag);
+            Console.WriteLine("Számok szorzata:");
+            Console.WriteLine(szorzata);
+   
+            Console.ReadKey();
+        }
+    }
+}
