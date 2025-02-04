562b56b6e4ccab3393731805f5b11f9fa535422d
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2025-02-04 14:21:58 +0100

    Fifth Project
    This is the fifth project

---
+++ Porjects/fifthproject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace fifthproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Tanulo informatika jegye");
+            int jegy = int.Parse(Console.ReadLine());
+
+            switch (jegy)
+            {
+                case 5:
+                {
+                    Console.WriteLine("Jeles");
+                    break;
+                }
+                case 4:
+                {
+                    Console.WriteLine("Jo");
+                    break;
+                }
+                case 3:
+                {
+                    Console.WriteLine("Kozepes");
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("Elegseges");
+                    break;
+                }
+                case 1:
+                {
+                    Console.WriteLine("Elegtelen");
+                    break;
+                }
+                default: 
+                    Console.WriteLine("Nincs ilyen osztalyzat!");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
